e78ecce6ff7e23b9748f7fb6a819ef69b4b09596
s1mph1we] <academy_intern@Academy-Interns-MacBook-Air-6.local>
2017-01-06 09:25:45 +0200

    add contend

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?PHP
 	echo "Hello Digital Academy";
+
+   class Product {
+    var $name;
+    function __construct($value) {
+        if (is_a($value, 'Product')) {
+            $this->name = $value->name;
+        } else {
+            $this->name = $value;
+        }
+    }
+}
 ?> 
